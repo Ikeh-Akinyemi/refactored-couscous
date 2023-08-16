4d3b84ef23fa1289f00f8ddc8b08d6bc313d1413
IkehAkinyemi <mrikehchukwuka@gmail.com>
2023-08-16 07:16:37 +0000

    still not sure what I'm doing

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,27 @@
-mod utils;
-
+extern crate wasm_bindgen;
 use wasm_bindgen::prelude::*;
 
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
 #[wasm_bindgen]
-extern "C" {
+extern {
     fn alert(s: &str);
 }
 
 #[wasm_bindgen]
 pub fn greet() {
-    alert("Hello, wasm-game-of-life!");
+    alert("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
 }
