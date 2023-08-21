3d996224094dc08ede2e589df2bf70b98e3df5f5
IkehAkinyemi <mrikehchukwuka@gmail.com>
2023-08-21 10:16:24 +0100

    refactor: convert to esmodule system

--- examples/server/src/index.js
+++
@@ -1,9 +0,0 @@
-import { Index } from 'refactored-couscous-v1';
-
-const index = new Index();
-index.add("Hello world");
-index.add("Rust is amazing");
-index.add("Wade in Rust");
-
-const results = index.search("rust");
-console.log(results); // ["Rust is amazing", "Wade in Rust"]
